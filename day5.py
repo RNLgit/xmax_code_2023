11b8065b1b505cd3157f14916da7eb51af10cb53
rnl <lirunnan90@gmail.com>
2023-12-06 09:07:28 +1100

    day5

--- day5.py
+++ day5.py
@@ -1,3 +1,25 @@
 def calc(seed: int, stop, start, distance) -> int:
     if int(seed) in range(int(start), int(distance)):
-        return int(stop) - int(start) + int(seed)
+        return int(seed) + int(stop) - int(start)
+    else:
+        return int(seed)
+
+
+def get_val(seed: int) -> int:
+    fh = open("day_5_data.txt", "r")
+    try:
+        while True:
+            line = next(fh)
+            if any(["\n" == line, "map" in line, "seed" in line]):
+                continue
+            else:
+                stop, start, distance = line.split()
+                seed = calc(seed, stop, start, distance)
+    except StopIteration:
+        return seed
+    finally:
+        fh.close()
+
+
+seeds = open("day_5_data.txt", "r").readline().split()[1:]
+print(sorted([get_val(seed) for seed in seeds])[0])
