c7d6daeeb16e0d70243781153da76c0d4463e4f5
runnan li <runnanli@runnans-MacBook-Pro.local>
2023-12-06 22:32:28 +1100

    d5

--- day5.py
+++ day5.py
@@ -1,25 +1,35 @@
+with open("day_5_data.txt", "r") as fh:
+    my_fh = fh.readlines()
+
+
 def calc(seed: int, stop, start, distance) -> int:
-    if int(seed) in range(int(start), int(distance)):
+    if int(seed) in range(int(start), int(start) + int(distance)):
         return int(seed) + int(stop) - int(start)
     else:
         return int(seed)
 
 
 def get_val(seed: int) -> int:
-    fh = open("day_5_data.txt", "r")
+    fh = iter(my_fh)
+    map_done = False
     try:
         while True:
             line = next(fh)
-            if any(["\n" == line, "map" in line, "seed" in line]):
+            if any(["\n" == line, "seed" in line]):
                 continue
+            if "map" in line:
+                map_done = False
             else:
+                if map_done:
+                    continue
                 stop, start, distance = line.split()
+                before_seed = seed
                 seed = calc(seed, stop, start, distance)
+                if before_seed != seed:
+                    map_done = True
     except StopIteration:
         return seed
-    finally:
-        fh.close()
 
 
-seeds = open("day_5_data.txt", "r").readline().split()[1:]
-print(sorted([get_val(seed) for seed in seeds])[0])
+seeds = [int(i) for i in open("day_5_data.txt", "r").readline().split()[1:]]
+print(min([get_val(int(seed)) for seed in seeds]))
